707b7104c166707aa10755447c5081d1bf2ec37b
Viktar BY <vik@vik.com>
2016-12-16 14:29:30 +0300

    Added new routes

--- routers.php
+++ routers.php
@@ -4,3 +4,6 @@
 $routers->get('task-list', 'TodoController@index');
 $routers->post('task/add', 'TodoController@add');
 $routers->post('task/update', 'TodoController@update');
+$routers->get('table', 'PageController@table');
+$routers->get('', 'PageController@index');
+$routers->get('about', 'PageController@about');
